93b66a6908f0ef526ace5c5882a9926bc653248d
yap <jiahaoyap9139@gmail.com>
2026-01-03 20:31:29 +0800

    update

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import "react-toastify/dist/ReactToastify.css";
 import "simplebar-react/dist/simplebar.min.css";
 import "flatpickr/dist/themes/light.css";
-import "react-svg-map/lib/index.css";
+// import "react-svg-map/lib/index.css";
 import "leaflet/dist/leaflet.css";
 import "./scss/app.scss";
 import { Provider } from "react-redux";
